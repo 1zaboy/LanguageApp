51f40a42a755f607a05e15623aaefa2bde25b1e5
Vlad <33203144+1zaboy@users.noreply.github.com>
2019-12-12 13:32:54 +0300

    new data

--- AiLan/AiLan/WorkWithDb.cs
+++ AiLan/AiLan/WorkWithDb.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.IO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Data.SQLite;
@@ -16,20 +17,29 @@ namespace AiLan
         {
             // Register the factory
             DbProviderFactories.RegisterFactory("System.Data.SQLite", SQLiteFactory.Instance);
-            ConnectionString = "Data Source = " + "./AllDb/DbForApp.db3";
+            ConnectionString = "Data Source = " + GetAbsolutePath("DbForApp.db3");
         }
 
         public IDataView GetDataFromSQLite(MLContext mlContext)
         {
             SQLiteFactory factory = (SQLiteFactory)DbProviderFactories.GetFactory("System.Data.SQLite");            
 
-            string sqlCommand = "SELECT Words.TextWord as 'Label', Language.Namelanguage as 'Message' from Words INNER JOIN Language ON Words.IdLanguage = Language.IdTable ORDER BY RANDOM() LIMIT 1";
+            string sqlCommand = "SELECT Words.TextWord as 'Label', Language.Namelanguage as 'Message' from Words INNER JOIN Language ON Words.IdLanguage = Language.IdTable ORDER BY RANDOM() LIMIT 100000";
 
             DatabaseSource dbSource = new DatabaseSource(factory, ConnectionString, sqlCommand);
 
             DatabaseLoader loader = mlContext.Data.CreateDatabaseLoader<wordInput>();
             IDataView data = loader.Load(dbSource);
             return data;
+        }
+        public string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+            return fullPath;
         }
     }
 }
